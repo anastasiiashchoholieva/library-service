a65410fc78be7a73164ee30852ad0da97dee3d99
Anastasiia <nastyashch1601@gmail.com>
2023-07-05 17:46:15 +0100

    Add Book model and migrate

--- books/models.py
+++ books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    COVER_CHOICES = [
+        ("HARD", "Hardcover"),
+        ("SOFT", "Softcover"),
+    ]
+
+    title = models.CharField(max_length=255, blank=False)
+    author = models.CharField(max_length=255, blank=False)
+    cover = models.CharField(max_length=4, choices=COVER_CHOICES)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.title
