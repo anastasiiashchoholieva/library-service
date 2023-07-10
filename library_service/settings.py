b65e797262140929ee2de3b0183e9384c3ea8e06
Anastasiia <nastyashch1601@gmail.com>
2023-07-10 13:12:14 +0100

    Modify celery settings

--- library_service/settings.py
+++ library_service/settings.py
@@ -154,15 +154,15 @@ SIMPLE_JWT = {
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-CELERY_BROKER_URL = "redis://localhost:6379/0"  # Update with your broker URL
-CELERY_ACCEPT_CONTENT = ["json"]
-CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_SERIALIZER = "json"
-CELERY_TIMEZONE = "UTC"
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_TIMEZONE = "Europe/Lisbon"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
 
 CELERY_BEAT_SCHEDULE = {
     "check_overdue_borrowings": {
-        "task": 'borrowings.tasks.check_overdue_borrowings',
+        "task": "borrowings.tasks.check_overdue_borrowings",
         "schedule": crontab(hour=0, minute=0),
     },
 }
