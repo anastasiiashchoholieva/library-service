4b5cb06d0400d8705cde533f30de31e7ba4910c0
Anastasiia <nastyashch1601@gmail.com>
2023-07-11 12:58:04 +0100

    Add fine calculation to payment_helper

--- helpers/payment_helper.py
+++ helpers/payment_helper.py
@@ -5,16 +5,30 @@ from django.urls import reverse
 
 from payments.models import Payment
 
+FINE_MULTIPLIER = 2
+
 
 def create_stripe_session(borrowing, request):
     book = borrowing.book
     daily_fee = book.daily_fee
     borrow_date = borrowing.borrow_date
     expected_return_date = borrowing.expected_return_date
+    actual_return_date = borrowing.actual_return_date
 
-    borrowed_days = (expected_return_date - borrow_date).days
-    total_price = daily_fee * borrowed_days
-    unit_amount = int(total_price * 100)
+    if actual_return_date is None:
+        borrowed_days = (expected_return_date - borrow_date).days
+        total_price = daily_fee * borrowed_days
+        unit_amount = int(total_price * 100)
+        payment_type = Payment.PaymentType.PAYMENT
+        money_to_pay = Decimal(total_price)
+
+    elif actual_return_date > expected_return_date:
+        daily_fee = borrowing.book.daily_fee
+        overdue_days = (borrowing.actual_return_date - borrowing.expected_return_date).days
+        fine_amount = overdue_days * daily_fee * FINE_MULTIPLIER
+        unit_amount = int(fine_amount * 100)
+        payment_type = Payment.PaymentType.FINE
+        money_to_pay = Decimal(fine_amount)
 
     stripe.api_key = settings.STRIPE_SECRET_KEY
     success_url = request.build_absolute_uri(reverse("payments:payment-success", kwargs={"session_id": "<session_id>"}))
@@ -42,11 +56,11 @@ def create_stripe_session(borrowing, request):
 
     payment = Payment.objects.create(
         status=Payment.PaymentStatus.PENDING,
-        type=Payment.PaymentType.PAYMENT,
-        borrowing_id=borrowing.id,
+        type=payment_type,
+        borrowing_id=borrowing,
         session_url=session.url,
         session_id=session.id,
-        money_to_pay=Decimal(total_price),
+        money_to_pay=money_to_pay,
     )
 
     return payment
