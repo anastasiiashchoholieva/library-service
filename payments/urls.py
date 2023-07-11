9d992e30fc4f95d7a2bf595744013eadaeba14b0
Anastasiia <nastyashch1601@gmail.com>
2023-07-11 11:24:42 +0100

    Implement success and cancel urls for Payment Service

--- payments/urls.py
+++ payments/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import PaymentSuccessView, PaymentCancelView
+from .views import PaymentSuccessView, PaymentCancelView, PaymentListView
 
 urlpatterns = [
-    path("success/", PaymentSuccessView.as_view(), name="success-payment"),
-    path("cancel/", PaymentCancelView.as_view(), name="cancel-payment"),
+    path("payments/", PaymentListView.as_view(), name="payment-list"),
+    path("payments/<str:session_id>/success/", PaymentSuccessView.as_view(), name="payment-success"),
+    path("payments/<str:session_id>/cancel/", PaymentCancelView.as_view(), name="payment-cancel"),
 ]
 
 app_name = "payments"
--- payments/views.py
+++ payments/views.py
@@ -1,14 +1,31 @@
+from django.http import HttpResponse
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from payments.models import Payment
 from payments.serializers import PaymentSerializer
 
 
-class PaymentSuccessView(generics.ListAPIView):
+class PaymentListView(generics.ListAPIView):
     queryset = Payment.objects.select_related()
     serializer_class = PaymentSerializer
 
 
-class PaymentCancelView(generics.RetrieveAPIView):
-    queryset = Payment.objects.select_related()
-    serializer_class = PaymentSerializer
+class PaymentSuccessView(APIView):
+    def get(self, request, session_id):
+        payment = Payment.objects.filter(session_id=session_id).first()
+        if payment and payment.status != Payment.PaymentStatus.PAID:
+            payment.status = Payment.PaymentStatus.PAID
+            payment.save()
+            return Response("Payment was successful")
+        return Response("Payment not found")
+
+
+class PaymentCancelView(APIView):
+    def get(self, request, session_id):
+        payment = Payment.objects.filter(session_id=session_id).first()
+        if payment and payment.status != Payment.PaymentStatus.PAID:
+            payment.status = Payment.PaymentStatus.CANCELLED
+            payment.save()
+            return Response("Payment can be paid a bit later")
